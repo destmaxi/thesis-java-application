00fc682c6039016852c7ec0a72c7e5efed5586ed
Maxime de Streel <maxime.destreel@student.uclouvain.be>
2020-02-21 15:16:49 +0100

    Read from /var/log/javaapp//data.log

--- src/main/java/com/UI/Application.java
+++ src/main/java/com/UI/Application.java
@@ -1,10 +1,27 @@
 package com.UI;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
 
 public class Application {
     public static void main(String[] args) {
-        ChartRender render = new ChartRender("test", "/var/log/javaapp/maxime/data.log");
+        Process proc = null;
+        try {
+            proc = Runtime.getRuntime().exec("hostname");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        BufferedReader stdInput = new BufferedReader(new InputStreamReader(Objects.requireNonNull(proc).getInputStream()));
+        String hostname = null;
+        try {
+            hostname = stdInput.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ChartRender render = new ChartRender("test", "/var/log/javaapp/" + hostname + "/data.log");
         try {
             render.render();
         } catch (FileNotFoundException e) {
